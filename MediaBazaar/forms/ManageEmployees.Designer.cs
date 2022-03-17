4d83649464d30e1f4f504edf5babf0ec1b6abe6f
Nikolay Valchanov <493454@student.fontys.nl>
2022-03-17 21:01:10 +0100

    Manage employees form redesign

--- MediaBazaar/forms/ManageEmployees.Designer.cs
+++ MediaBazaar/forms/ManageEmployees.Designer.cs
@@ -29,6 +29,8 @@ namespace MediaBazaar
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManageEmployees));
             this.lvEmps = new System.Windows.Forms.ListView();
             this.colID = new System.Windows.Forms.ColumnHeader();
             this.colUsername = new System.Windows.Forms.ColumnHeader();
@@ -42,13 +44,21 @@ namespace MediaBazaar
             this.colEmail = new System.Windows.Forms.ColumnHeader();
             this.colPhone = new System.Windows.Forms.ColumnHeader();
             this.colIsStudent = new System.Windows.Forms.ColumnHeader();
-            this.btnAddEmployee = new System.Windows.Forms.Button();
-            this.btnRemoveEmployee = new System.Windows.Forms.Button();
-            this.btnEditInfo = new System.Windows.Forms.Button();
-            this.btnViewShifts = new System.Windows.Forms.Button();
-            this.btnManageShifts = new System.Windows.Forms.Button();
             this.tbSearchEmps = new System.Windows.Forms.TextBox();
             this.btnSearchEmps = new System.Windows.Forms.Button();
+            this.pbxAdd = new System.Windows.Forms.PictureBox();
+            this.pbxRemove = new System.Windows.Forms.PictureBox();
+            this.pbxEdit = new System.Windows.Forms.PictureBox();
+            this.pbxManageShift = new System.Windows.Forms.PictureBox();
+            this.pbxSchedule = new System.Windows.Forms.PictureBox();
+            this.pnlTools = new System.Windows.Forms.Panel();
+            this.ttpMain = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxAdd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRemove)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxManageShift)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSchedule)).BeginInit();
+            this.pnlTools.SuspendLayout();
             this.SuspendLayout();
             // 
             // lvEmps
@@ -71,10 +81,10 @@ namespace MediaBazaar
             this.lvEmps.FullRowSelect = true;
             this.lvEmps.GridLines = true;
             this.lvEmps.HideSelection = false;
-            this.lvEmps.Location = new System.Drawing.Point(12, 54);
+            this.lvEmps.Location = new System.Drawing.Point(14, 56);
             this.lvEmps.MultiSelect = false;
             this.lvEmps.Name = "lvEmps";
-            this.lvEmps.Size = new System.Drawing.Size(1211, 315);
+            this.lvEmps.Size = new System.Drawing.Size(1105, 350);
             this.lvEmps.TabIndex = 1;
             this.lvEmps.UseCompatibleStateImageBehavior = false;
             this.lvEmps.View = System.Windows.Forms.View.Details;
@@ -127,56 +137,6 @@ namespace MediaBazaar
             // 
             this.colIsStudent.Text = "Student";
             // 
-            // btnAddEmployee
-            // 
-            this.btnAddEmployee.Location = new System.Drawing.Point(21, 396);
-            this.btnAddEmployee.Name = "btnAddEmployee";
-            this.btnAddEmployee.Size = new System.Drawing.Size(150, 53);
-            this.btnAddEmployee.TabIndex = 2;
-            this.btnAddEmployee.Text = "Add Employee";
-            this.btnAddEmployee.UseVisualStyleBackColor = true;
-            this.btnAddEmployee.Click += new System.EventHandler(this.btnAddEmployee_Click);
-            // 
-            // btnRemoveEmployee
-            // 
-            this.btnRemoveEmployee.Location = new System.Drawing.Point(190, 396);
-            this.btnRemoveEmployee.Name = "btnRemoveEmployee";
-            this.btnRemoveEmployee.Size = new System.Drawing.Size(155, 53);
-            this.btnRemoveEmployee.TabIndex = 3;
-            this.btnRemoveEmployee.Text = "Remove selected employee";
-            this.btnRemoveEmployee.UseVisualStyleBackColor = true;
-            this.btnRemoveEmployee.Click += new System.EventHandler(this.btnRemoveEmployee_Click);
-            // 
-            // btnEditInfo
-            // 
-            this.btnEditInfo.Location = new System.Drawing.Point(364, 396);
-            this.btnEditInfo.Name = "btnEditInfo";
-            this.btnEditInfo.Size = new System.Drawing.Size(155, 53);
-            this.btnEditInfo.TabIndex = 4;
-            this.btnEditInfo.Text = "Edit info";
-            this.btnEditInfo.UseVisualStyleBackColor = true;
-            this.btnEditInfo.Click += new System.EventHandler(this.btnEditInfo_Click);
-            // 
-            // btnViewShifts
-            // 
-            this.btnViewShifts.Location = new System.Drawing.Point(190, 471);
-            this.btnViewShifts.Name = "btnViewShifts";
-            this.btnViewShifts.Size = new System.Drawing.Size(150, 53);
-            this.btnViewShifts.TabIndex = 5;
-            this.btnViewShifts.Text = "View work shifts and attendance";
-            this.btnViewShifts.UseVisualStyleBackColor = true;
-            this.btnViewShifts.Click += new System.EventHandler(this.btnViewShifts_Click);
-            // 
-            // btnManageShifts
-            // 
-            this.btnManageShifts.Location = new System.Drawing.Point(15, 471);
-            this.btnManageShifts.Name = "btnManageShifts";
-            this.btnManageShifts.Size = new System.Drawing.Size(155, 53);
-            this.btnManageShifts.TabIndex = 6;
-            this.btnManageShifts.Text = "Manage shifts for employee";
-            this.btnManageShifts.UseVisualStyleBackColor = true;
-            this.btnManageShifts.Click += new System.EventHandler(this.btnManageShifts_Click);
-            // 
             // tbSearchEmps
             // 
             this.tbSearchEmps.Location = new System.Drawing.Point(15, 12);
@@ -190,26 +150,129 @@ namespace MediaBazaar
             this.btnSearchEmps.Name = "btnSearchEmps";
             this.btnSearchEmps.Size = new System.Drawing.Size(94, 29);
             this.btnSearchEmps.TabIndex = 8;
-            this.btnSearchEmps.Text = "Search";
+            this.btnSearchEmps.Text = "🔍Search";
             this.btnSearchEmps.UseVisualStyleBackColor = true;
             this.btnSearchEmps.Click += new System.EventHandler(this.btnSearchEmps_Click);
+            // 
+            // pbxAdd
+            // 
+            this.pbxAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbxAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxAdd.Image = ((System.Drawing.Image)(resources.GetObject("pbxAdd.Image")));
+            this.pbxAdd.Location = new System.Drawing.Point(14, 11);
+            this.pbxAdd.Name = "pbxAdd";
+            this.pbxAdd.Size = new System.Drawing.Size(60, 60);
+            this.pbxAdd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxAdd.TabIndex = 9;
+            this.pbxAdd.TabStop = false;
+            this.ttpMain.SetToolTip(this.pbxAdd, "Add new employee");
+            this.pbxAdd.Click += new System.EventHandler(this.btnAddEmployee_Click);
+            this.pbxAdd.MouseEnter += new System.EventHandler(this.TogglePictureButtonSize);
+            this.pbxAdd.MouseLeave += new System.EventHandler(this.TogglePictureButtonSize);
+            // 
+            // pbxRemove
+            // 
+            this.pbxRemove.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbxRemove.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxRemove.Image = ((System.Drawing.Image)(resources.GetObject("pbxRemove.Image")));
+            this.pbxRemove.Location = new System.Drawing.Point(268, 11);
+            this.pbxRemove.Name = "pbxRemove";
+            this.pbxRemove.Padding = new System.Windows.Forms.Padding(2);
+            this.pbxRemove.Size = new System.Drawing.Size(60, 60);
+            this.pbxRemove.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxRemove.TabIndex = 9;
+            this.pbxRemove.TabStop = false;
+            this.ttpMain.SetToolTip(this.pbxRemove, "Remove selected employee");
+            this.pbxRemove.Click += new System.EventHandler(this.btnRemoveEmployee_Click);
+            this.pbxRemove.MouseEnter += new System.EventHandler(this.TogglePictureButtonSize);
+            this.pbxRemove.MouseLeave += new System.EventHandler(this.TogglePictureButtonSize);
+            // 
+            // pbxEdit
+            // 
+            this.pbxEdit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbxEdit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxEdit.Image = ((System.Drawing.Image)(resources.GetObject("pbxEdit.Image")));
+            this.pbxEdit.Location = new System.Drawing.Point(522, 11);
+            this.pbxEdit.Name = "pbxEdit";
+            this.pbxEdit.Padding = new System.Windows.Forms.Padding(2);
+            this.pbxEdit.Size = new System.Drawing.Size(60, 60);
+            this.pbxEdit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxEdit.TabIndex = 9;
+            this.pbxEdit.TabStop = false;
+            this.ttpMain.SetToolTip(this.pbxEdit, "Edit selected employee");
+            this.pbxEdit.Click += new System.EventHandler(this.btnEditInfo_Click);
+            this.pbxEdit.MouseEnter += new System.EventHandler(this.TogglePictureButtonSize);
+            this.pbxEdit.MouseLeave += new System.EventHandler(this.TogglePictureButtonSize);
+            // 
+            // pbxManageShift
+            // 
+            this.pbxManageShift.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbxManageShift.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxManageShift.Image = ((System.Drawing.Image)(resources.GetObject("pbxManageShift.Image")));
+            this.pbxManageShift.Location = new System.Drawing.Point(776, 11);
+            this.pbxManageShift.Name = "pbxManageShift";
+            this.pbxManageShift.Padding = new System.Windows.Forms.Padding(2);
+            this.pbxManageShift.Size = new System.Drawing.Size(60, 60);
+            this.pbxManageShift.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxManageShift.TabIndex = 9;
+            this.pbxManageShift.TabStop = false;
+            this.ttpMain.SetToolTip(this.pbxManageShift, "Schedule new shift");
+            this.pbxManageShift.Click += new System.EventHandler(this.btnManageShifts_Click);
+            this.pbxManageShift.MouseEnter += new System.EventHandler(this.TogglePictureButtonSize);
+            this.pbxManageShift.MouseLeave += new System.EventHandler(this.TogglePictureButtonSize);
+            // 
+            // pbxSchedule
+            // 
+            this.pbxSchedule.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbxSchedule.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbxSchedule.Image = ((System.Drawing.Image)(resources.GetObject("pbxSchedule.Image")));
+            this.pbxSchedule.Location = new System.Drawing.Point(1030, 11);
+            this.pbxSchedule.Name = "pbxSchedule";
+            this.pbxSchedule.Size = new System.Drawing.Size(60, 60);
+            this.pbxSchedule.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxSchedule.TabIndex = 9;
+            this.pbxSchedule.TabStop = false;
+            this.ttpMain.SetToolTip(this.pbxSchedule, "View shifts and manage attendance");
+            this.pbxSchedule.Click += new System.EventHandler(this.btnViewShifts_Click);
+            this.pbxSchedule.MouseEnter += new System.EventHandler(this.TogglePictureButtonSize);
+            this.pbxSchedule.MouseLeave += new System.EventHandler(this.TogglePictureButtonSize);
+            // 
+            // pnlTools
+            // 
+            this.pnlTools.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlTools.BackColor = System.Drawing.Color.PeachPuff;
+            this.pnlTools.Controls.Add(this.pbxAdd);
+            this.pnlTools.Controls.Add(this.pbxSchedule);
+            this.pnlTools.Controls.Add(this.pbxRemove);
+            this.pnlTools.Controls.Add(this.pbxManageShift);
+            this.pnlTools.Controls.Add(this.pbxEdit);
+            this.pnlTools.Location = new System.Drawing.Point(15, 427);
+            this.pnlTools.Name = "pnlTools";
+            this.pnlTools.Size = new System.Drawing.Size(1104, 80);
+            this.pnlTools.TabIndex = 10;
             // 
             // ManageEmployees
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1235, 589);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1133, 522);
+            this.Controls.Add(this.pnlTools);
             this.Controls.Add(this.btnSearchEmps);
             this.Controls.Add(this.tbSearchEmps);
-            this.Controls.Add(this.btnManageShifts);
-            this.Controls.Add(this.btnViewShifts);
-            this.Controls.Add(this.btnEditInfo);
-            this.Controls.Add(this.btnRemoveEmployee);
-            this.Controls.Add(this.btnAddEmployee);
             this.Controls.Add(this.lvEmps);
+            this.MinimumSize = new System.Drawing.Size(1151, 569);
             this.Name = "ManageEmployees";
+            this.Padding = new System.Windows.Forms.Padding(2);
             this.Text = "ManageEmployees";
             this.Load += new System.EventHandler(this.ManageEmployees_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxAdd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRemove)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxManageShift)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSchedule)).EndInit();
+            this.pnlTools.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,14 +290,16 @@ namespace MediaBazaar
         private System.Windows.Forms.ColumnHeader colEmail;
         private System.Windows.Forms.ColumnHeader colPhone;
         private System.Windows.Forms.ColumnHeader colWage;
-        private System.Windows.Forms.Button btnAddEmployee;
-        private System.Windows.Forms.Button btnRemoveEmployee;
-        private System.Windows.Forms.Button btnEditInfo;
-        private System.Windows.Forms.Button btnViewShifts;
-        private System.Windows.Forms.Button btnManageShifts;
         private System.Windows.Forms.ColumnHeader colDepName;
         private System.Windows.Forms.ColumnHeader colIsStudent;
         private System.Windows.Forms.TextBox tbSearchEmps;
         private System.Windows.Forms.Button btnSearchEmps;
+        private System.Windows.Forms.PictureBox pbxAdd;
+        private System.Windows.Forms.PictureBox pbxRemove;
+        private System.Windows.Forms.PictureBox pbxEdit;
+        private System.Windows.Forms.PictureBox pbxManageShift;
+        private System.Windows.Forms.PictureBox pbxSchedule;
+        private System.Windows.Forms.Panel pnlTools;
+        private System.Windows.Forms.ToolTip ttpMain;
     }
 }

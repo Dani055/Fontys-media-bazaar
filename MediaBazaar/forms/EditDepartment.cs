200a74224459f2a1caa6b632c14c2311038cb3f7
Nikolay Valchanov <493454@student.fontys.nl>
2022-04-05 11:52:24 +0200

    Quick fix.

--- MediaBazaar/forms/EditDepartment.cs
+++ MediaBazaar/forms/EditDepartment.cs
@@ -29,7 +29,6 @@ namespace MediaBazaar.forms
         private void EditDepartment_Load(object sender, EventArgs e)
         {           
             tbxNewName.Text = department.Name;
-            chbxEssential.Checked = department.IsEssential;
         }
 
         private void btnEditDepartment_Click(object sender, EventArgs e)

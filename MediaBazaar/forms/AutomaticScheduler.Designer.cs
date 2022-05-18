d020be74dfef384d91e9dc8faa1ced9078a4f30a
Tudor Morar <478896@student.fontys.nl>
2022-05-18 18:10:35 +0200

    move autoscheduler logic to a separate class. removed manager scheduling

--- MediaBazaar/forms/AutomaticScheduler.Designer.cs
+++ MediaBazaar/forms/AutomaticScheduler.Designer.cs
@@ -31,10 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutomaticScheduler));
             this.lblWarningMessage = new System.Windows.Forms.Label();
             this.lblContinue = new System.Windows.Forms.Label();
-            this.btnYes = new System.Windows.Forms.Button();
-            this.btnNo = new System.Windows.Forms.Button();
             this.lblNextWeek = new System.Windows.Forms.Label();
             this.lblDates = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblWarningMessage
@@ -57,26 +57,6 @@
             this.lblContinue.TabIndex = 1;
             this.lblContinue.Text = "Do you wish to continue?";
             // 
-            // btnYes
-            // 
-            this.btnYes.Location = new System.Drawing.Point(216, 260);
-            this.btnYes.Name = "btnYes";
-            this.btnYes.Size = new System.Drawing.Size(88, 45);
-            this.btnYes.TabIndex = 2;
-            this.btnYes.Text = "Yes";
-            this.btnYes.UseVisualStyleBackColor = true;
-            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
-            // 
-            // btnNo
-            // 
-            this.btnNo.Location = new System.Drawing.Point(460, 260);
-            this.btnNo.Name = "btnNo";
-            this.btnNo.Size = new System.Drawing.Size(88, 45);
-            this.btnNo.TabIndex = 3;
-            this.btnNo.Text = "No";
-            this.btnNo.UseVisualStyleBackColor = true;
-            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
-            // 
             // lblNextWeek
             // 
             this.lblNextWeek.AutoSize = true;
@@ -95,17 +75,52 @@
             this.lblDates.TabIndex = 5;
             this.lblDates.Text = "<insert following week>";
             // 
+            // btnYes
+            // 
+            this.btnYes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnYes.FlatAppearance.BorderColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnYes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnYes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYes.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnYes.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnYes.Location = new System.Drawing.Point(191, 308);
+            this.btnYes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(131, 61);
+            this.btnYes.TabIndex = 7;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNo.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNo.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnNo.Location = new System.Drawing.Point(445, 308);
+            this.btnNo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(131, 61);
+            this.btnNo.TabIndex = 8;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
             // AutomaticScheduler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblDates);
-            this.Controls.Add(this.lblNextWeek);
             this.Controls.Add(this.btnNo);
             this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.lblDates);
+            this.Controls.Add(this.lblNextWeek);
             this.Controls.Add(this.lblContinue);
             this.Controls.Add(this.lblWarningMessage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AutomaticScheduler";
             this.Text = "AutomaticScheduler";
             this.ResumeLayout(false);
@@ -117,9 +132,9 @@
 
         private System.Windows.Forms.Label lblWarningMessage;
         private System.Windows.Forms.Label lblContinue;
-        private System.Windows.Forms.Button btnYes;
-        private System.Windows.Forms.Button btnNo;
         private System.Windows.Forms.Label lblNextWeek;
         private System.Windows.Forms.Label lblDates;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
     }
 }
